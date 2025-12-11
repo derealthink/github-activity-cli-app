9ea515225d6271724105bc7c293d45ebd1d4c329
umberto <u.ponci@gmail.com>
2025-12-11 16:39:56 +0100

    added the beginning of a fetch data function

--- index.js
+++ index.js
@@ -2,5 +2,28 @@
 
 const username = process.argv[2];
 
-console.log(username);
+if (!username)
+{
+     console.log("please enter a username");
+     return ;
+}
 
+async function fetchData(username)
+{
+    const endpoint = `https://api.github.com/users/${username}/events`;
+
+    try 
+    {
+        const response = fetch(endpoint);
+        if (!response.ok){
+            throw new Error(`Error status ${response.status}`)
+        }
+        const result = await response.json();
+        console.log(result);
+    }
+    catch (error) 
+    {
+        console.error(error.message)
+    }
+}
+fetchData(username);
